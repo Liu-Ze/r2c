567e83fb7bace1229f5a31096e0923e4b65f27cd
LiuZe <liuze@mail.ustc.edu.cn>
2019-01-16 17:06:36 +0800

    add init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo /opt/conda/bin/conda install pytorch -c pytorch
 pip install git+git://github.com/pytorch/vision.git@24577864e92b72f7066e1ed16e978e873e19d13d --user
 pip install -r allennlp-requirements.txt --user
 pip install --no-deps allennlp==0.8.0 --user
-sudo opt/conda/bin/python -m spacy download en_core_web_sm
+sudo /opt/conda/bin/python -m spacy download en_core_web_sm
 # this one is optional but it should help make things faster
 sudo /opt/conda/bin/pip uninstall pillow && CC="cc -mavx2" sudo /opt/conda/bin/pip install -U --force-reinstall pillow-simd
 
