0def054a834d7f40ccfe5789cf9d3ec18cea676e
LiuZe <liuze@mail.ustc.edu.cn>
2019-01-21 16:00:24 +0800

    update cfg

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ USE_IMAGENET_PRETRAINED = True # otherwise use detectron, but that doesnt seem t
 
 # Change these to match where your annotations and images are
 VCR_IMAGES_DIR = '/philly/rr1/resrchvc_scratch/v-wesu/data/vcr/vcr1images.zip@vcr1images'
-VCR_ANNOTS_DIR = '/philly/rr1/resrchvc_scratch/v-wesu/data/vcr'
+VCR_ANNOTS_DIR = '/philly/rr1/resrchvc_scratch/v-wesu/data/vcr/'
