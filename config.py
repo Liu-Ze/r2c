1b363c2e87a4619944a06be9f761002dbdbce832
LiuZe <liuze@mail.ustc.edu.cn>
2019-01-21 11:24:40 +0800

    Revert "add init.sh"
    This reverts commit f5223cd

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 USE_IMAGENET_PRETRAINED = True # otherwise use detectron, but that doesnt seem to work?!?
 
 # Change these to match where your annotations and images are
-VCR_IMAGES_DIR = '/hdfs/msrlabs/v-lize/data/vcr/vcr1images.zip@vcr1images'
+VCR_IMAGES_DIR = '/hdfs/msrlabs/v-lize/data/vcr/vcr1images_new.zip@vcr1images'
 VCR_ANNOTS_DIR = '/hdfs/msrlabs/v-lize/data/vcr'
